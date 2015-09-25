92d2d4d7f415f89500297bc4ffa5e55224b8ce9d
Michael Faille <michael@faille.io>
2015-09-25 01:51:00 -0400

    ADD timeout again for hbase warmup

--- conf/start-opentsdb.sh
+++ conf/start-opentsdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Source : https://github.com/PeterGrace/opentsdb-docker/blob/master/start_opentsdb.sh#L2
 echo "Sleeping for 30 seconds to give HBase time to warm up"
+sleep 30
 stopwaitsecs=60
 
 if [ ! -e /data/persistant/opentsdb_tables_created.txt ]; then
